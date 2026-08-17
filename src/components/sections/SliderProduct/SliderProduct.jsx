3293cc734cfe7c5e8ee90fc3183de7f198c5e444
johnsame1 <129014486+johnsame1@users.noreply.github.com>
2026-08-18 02:39:40 +0300

    vercel

--- src/components/sections/SliderProduct/SliderProduct.jsx
+++ src/components/sections/SliderProduct/SliderProduct.jsx
@@ -4,7 +4,7 @@ import useEmblaCarousel from "embla-carousel-react";
 import AutoScroll from "embla-carousel-auto-scroll";
 
 import "./SliderProduct.css";
-import { PRODUCTS } from "../SliderProduct/productsData.Js";
+import { PRODUCTS } from "./productsData.Js";
 
 const TYPES = [
   "marble",
